a7b14276057ec86f57f78443ccc6b558006c95fe
Oleg Apostol <truerenton@gmail.com>
2019-07-16 19:29:19 +0300

    feat: <FileUploader/>

---
+++ src/components/FileUploader/FileUploader.jsx
@@ -0,0 +1,112 @@
+import React, { Component } from 'react'
+import { store, view } from 'react-easy-state'
+import cn from 'classnames'
+import { bind } from 'decko'
+import nanoid from 'nanoid'
+
+import upload from 'tools/xhrLoad'
+
+import Input from 'components/UI/Input'
+
+import s from './FileUploader.styl'
+
+class FileUploader extends Component {
+  store = store({
+    total: 0,
+    loaded: 0,
+    progress: false,
+    complete: false,
+    error: false
+  });
+
+  @bind
+  async onChange(e) {
+    Object.assign(this.store, {
+      total: 1,
+      loaded: 0,
+      complete: false,
+      progress: true,
+      error: false
+    });
+
+    const { files } = e.target;
+
+    if (files.length === 0) {
+      return;
+    }
+
+    try {
+      await this.upload(files[0]);
+    } catch(err) {
+      Object.assign(this.store, {
+        progress: false,
+        error: true
+      });
+    }
+  };
+
+  @bind
+  onProgress({ loaded, total }) {
+    const complete = loaded === total;
+
+    Object.assign(this.store, {
+      total,
+      loaded,
+      complete,
+      progress: !complete,
+    });
+  }
+
+  async upload(file) {
+    const { prefix, onUpload } = this.props;
+    const formData = new FormData();
+    const ext = file.type.split('/')[1];
+    const fileName = `${prefix}/${nanoid()}.${ext}`;
+
+    formData.append('file', file);
+
+    const params = {
+      method: 'POST',
+      body: formData,
+      headers: {
+        'x-filename': fileName,
+      }
+    };
+
+    await upload('/upload', params, this.onProgress);
+    Object.assign(this.store, {
+      progress: false,
+      complete: true
+    });
+
+    onUpload(`https://${DO_SPACE_NAME}.${DO_SPACE_NS}/${fileName}`);
+  }
+
+  render() {
+    const { className } = this.props;
+    const { progress, complete, error, total, loaded } = this.store;
+
+    const classes = cn(s.root, className);
+    const indicatorClasses = cn(
+      s.progress,
+      complete && (error ? s.error : s.success)
+    );
+
+    return (
+      <div className={classes}>
+        <Input
+          type="file"
+          onChange={this.onChange}
+          placeholder="Choose file"
+          loading={progress}
+        />
+        <div
+          className={indicatorClasses}
+          style={{ left: `${loaded / total * 100 -100}%` }}
+        />
+      </div>
+    );
+  }
+}
+
+export default view(FileUploader);
---
+++ src/tools/xhrLoad.js
@@ -0,0 +1,40 @@
+/**
+ * Uploading with onProgress callback
+ * https://github.com/github/fetch/issues/89
+ *
+ * @param  {String} url
+ * @param  {String} options.method
+ * @param  {Object} options.headers
+ * @param  {*} options.body
+ * @param  {Function} onProgress
+ *
+ * @return {Promise}
+ */
+export default function(url, { method, headers, body }, onProgress) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    const hKeys = Object.keys(Object(headers));
+
+    xhr.open(method, url);
+    hKeys.forEach(h => xhr.setRequestHeader(h, headers[h]));
+    xhr.onreadystatechange = () => {
+      const { readyState, status } = xhr;
+
+      if (readyState === XMLHttpRequest.DONE) {
+        [200, 201].includes(status)
+          ? resolve(xhr)
+          : reject(xhr);
+      }
+    };
+
+    if (onProgress) {
+      if (method === 'GET') {
+        xhr.onprogress = onProgress;
+      } else {
+        xhr.upload.onprogress = onProgress;
+      }
+    }
+
+    xhr.send(body);
+  });
+}
