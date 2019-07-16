057e7656741273c248b976e9023e8f78339ab0be
Oleg Apostol <truerenton@gmail.com>
2019-07-16 18:45:58 +0300

    feat: User add admin menu, avatar icon; use new UI components

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,33 +1,69 @@
-import React, { Fragment } from 'react';
-import { view } from 'react-easy-state';
-import Link from 'components/UI/Link';
+import React from 'react'
+import { view } from 'react-easy-state'
 
-import UserStore from 'store/user';
-import PageStore from 'store/page';
+import UserStore from 'store/user'
+import PageStore from 'store/page'
 
-import s from './User.styl';
+import Link from 'components/UI/Link'
+import Popup from 'components/UI/Popup'
+import Menu, { MenuItem } from 'components/UI/Menu'
 
-function LoggedContent({ name, email }) {
+import DefaultIcon from './icons/avatar.svg'
+import s from './User.styl'
+
+function Userpic({ url }) {
+  if (!url) {
+    return <DefaultIcon className={s.avatar} />
+  }
+
   return (
-    <Fragment>
-      <Link to="/profile">{email || name}</Link>
-      &nbsp;
-      <Link to="/logout">Logout</Link>
-    </Fragment>
+    <div
+      className={s.avatar}
+      style={{ backgroundImage: `url(${url})` }}
+    />
   );
 }
 
+function getMenuLinks({ isAuth, isLogged, isAdmin }) {
+  const items = [];
+
+  if (isLogged) {
+    if (isAdmin) {
+      items.push(['/admin', 'Admin panel']);
+    }
+
+    items.push(
+      ['/profile', 'Profile'],
+      ['/logout', 'Logout']
+    );
+  } else if (!isAuth) {
+    items.push(['/login', 'Sign in']);
+  }
+
+  return items;
+}
+
 function User() {
-  const { isLogged, name, email } = UserStore;
+  const { isLogged, isAdmin, email, name, avatar } = UserStore;
   const { isAuth } = PageStore;
+  const items = getMenuLinks({ isLogged, isAdmin, isAuth });
 
   return (
-    <div className={s.root}>
-      {isLogged
-        ? view(LoggedContent({ name, email }))
-        : (!isAuth && <Link to="/login">Sign in</Link>)
-      }
-    </div>
+    <Popup autoClose>
+      <div className={s.user}>
+        {email || name || 'Guest'}
+        <Userpic url={avatar} />
+      </div>
+      {items.length > 0 && (
+        <Menu className={s.menu} align="right">
+          {items.map(([path, text]) => (
+            <MenuItem key={path}>
+              <Link to={path}>{text}</Link>
+            </MenuItem>
+          ))}
+        </Menu>
+      )}
+    </Popup>
   );
 }
 
